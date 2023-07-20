9acb68d3ef585aab424d5488e7495e2c86c09159
Shystra <jlsp.2019@hotmail.com>
2023-07-19 23:13:08 -0300

    feate: just links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,12 @@ import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
+  const urlInstagram = "https://www.instagram.com/httpsluucas/";
+  const urlFacebook = 'https://www.facebook.com/joaolucas.pereira.121';
+  const linkDin = 'https://www.linkedin.com/in/jo%C3%A3o-lucas-souza-a94193229/';
+
+
+
   return (
     <footer className="footer">
       <Container>
@@ -16,9 +22,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href={linkDin}><img src={navIcon1} alt="Icon" /></a>
+              <a href={urlFacebook}><img src={navIcon2} alt="Icon" /></a>
+              <a href={urlInstagram}><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p>Copyright 2023. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,6 +11,10 @@ import {
 
 export const NavBar = () => {
 
+  const urlInstagram = "https://www.instagram.com/httpsluucas/";
+  const urlFacebook = 'https://www.facebook.com/joaolucas.pereira.121';
+  const linkDin = 'https://www.linkedin.com/in/jo%C3%A3o-lucas-souza-a94193229/';
+
   const [activeLink, setActiveLink] = useState('home');
   const [scrolled, setScrolled] = useState(false);
 
@@ -50,9 +54,9 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href={linkDin}><img src={navIcon1} alt="" /></a>
+                <a href={urlFacebook}><img src={navIcon2} alt="" /></a>
+                <a href={urlInstagram}><img src={navIcon3} alt="" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
