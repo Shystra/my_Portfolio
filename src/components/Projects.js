f90c6895b9733df530470eb9e344883b28f27f45
Shystra <jlsp.2019@hotmail.com>
2023-07-25 13:00:16 -0300

    just project 4

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from '../assets/img/project-img4.png';
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
@@ -26,6 +27,11 @@ export const Projects = () => {
       title: "Pokemon",
       description: "This was my first project utilizing a serverless provider. In future, i pretend to change this aplication for other API.",
       imgUrl: projImg3,
+    },
+    {
+      title: "Tasks",
+      description: "This project to render just tasks for users. You can to change, delete and to add",
+      imgUrl: projImg4,
     },
     // {
     //   title: "Business Startup",
